076766b80f9eb0cf7a03d03426219af3226e5c28
uncledb <434967007@qq.com>
2015-03-15 21:33:34 +0800

    add tree traverse

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -1,5 +1,38 @@
 package tree;
 
 public class Node {
-		
+	private Node leftNode;
+	private Node rightNode;
+	private String data;
+
+	public Node(Node l, String d, Node r) {
+		this.leftNode = l;
+		this.rightNode = r;
+		this.data = d;
+	}
+
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
 }
--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,5 +1,74 @@
 package tree;
 
 public class Tree {
+	private Node rootNode = new Node(null, "1", null);
 
+	public static void main(String[] args) {
+		Tree t = new Tree();
+		System.out.println("前序遍历：");
+		t.preOrderTraverse(t.getRootNode());
+		System.out.println();
+		System.out.println("中序遍历：");
+		t.inOrderTraverse(t.getRootNode());
+		System.out.println();
+		System.out.println("后序遍历：");
+		t.postOrderPrint(t.getRootNode());
+	}
+
+	public Node getRootNode() {
+		return rootNode;
+	}
+
+	public void setRootNode(Node rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	public Tree() {
+		Node node8 = new Node(null, "8", null);
+		Node node7 = new Node(null, "7", null);
+		Node node6 = new Node(null, "6", null);
+		Node node5 = new Node(null, "5", null);
+		Node node4 = new Node(node8, "4", null);
+		Node node3 = new Node(node6, "3", node7);
+		Node node2 = new Node(node4, "2", node5);
+		this.rootNode = new Node(node2, "1", node3);
+	}
+
+	/**
+	 * 前序遍历
+	 */
+	public void preOrderTraverse(Node node) {
+		// 先遍历根节点 然后前序遍历左子树 再前序遍历右子树
+		// 前序：先根结点 再左子树 再右子树
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.getData() + ",");
+		preOrderTraverse(node.getLeftNode());
+		preOrderTraverse(node.getRightNode());
+	}
+
+	/**
+	 * 中序遍历
+	 */
+	public void inOrderTraverse(Node node) {
+		if (node == null) {
+			return;
+		}
+		inOrderTraverse(node.getLeftNode());
+		System.out.print(node.getData() + ",");
+		inOrderTraverse(node.getRightNode());
+	}
+
+	/**
+	 * 后序遍历
+	 */
+	public void postOrderPrint(Node node) {
+		if (node == null) {
+			return;
+		}
+		postOrderPrint(node.getLeftNode());
+		postOrderPrint(node.getRightNode());
+		System.out.print(node.getData() + ",");
+	}
 }
